533d9fb79218a7181ffba9d37b7e8eb2eacca4d4
TheBlueSoul5188 <hibba04@hotmail.se>
2022-10-07 10:44:19 +0200

    Add project files.

---
+++ While loopar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift4_1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilket är världens folkrikaste land?");
+            string svar = Console.ReadLine();
+
+            while(svar != "Kina")
+            {
+                Console.WriteLine("Du svarade fel, försök igen");
+                svar = Console.ReadLine(); 
+            }
+            Console.WriteLine("Du svarade rätt");
+        }
+    }
+}
